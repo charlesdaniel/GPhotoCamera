526ab7a3d815b623424ed103e08eb7288defe178
Charles Daniel <cdaniel@omniti.com>
2011-09-12 14:02:00 -0400

    minor fix

--- GPhotoCamera.py
+++ GPhotoCamera.py
@@ -12,7 +12,7 @@
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Paparazzi; see the file COPYING.  If not, write to
+## along with GPhotoCamera; see the file COPYING.  If not, write to
 ## the Free Software Foundation, 59 Temple Place - Suite 330,
 ## Boston, MA 02111-1307, USA.
 
